3976600d377fdeb6e9c9f0ed14178d3d0263f3f9
Totalinfonet <totalinfonet@gmail.com>
2023-03-04 20:12:27 +0200

    Profile componennt added

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import { Container } from './App.styled';
+import { Profile } from 'components/Profile/Profile';
+import user from '../../user.json';
+
+export const App = () => {
+  return (
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+      />
+    </Container>
+  );
+};
